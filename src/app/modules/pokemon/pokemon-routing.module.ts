90a881ec77e6933ee30472a49d8c03ac6853b001
Leonel Cordova Domagalski <domagalski.leonel@gmail.com>
2021-12-08 17:51:50 -0300

    continuação do controller do Pokemon

--- src/app/modules/pokemon/pokemon-routing.module.ts
+++ src/app/modules/pokemon/pokemon-routing.module.ts
@@ -1,22 +1,20 @@
+import { createComponent } from '@angular/compiler/src/core';
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TipoListaComponent } from '../tipo-lista/tipo-lista.component';
-
 
 @Component({
   template: '<router-outlet></router-outlet>',
 })
-export class GenericRouterComponent {}
-// variavel do tipo Routes é uma interface do angular que se torna um objeto
+export class GenericRouterComponent {};
+// variavel do tipo Route é uma interface do angular que se torna um objeto
+
 const routes: Routes = [
   {
-
     path: 'pokemon',
 // quando feito um get em pokemons, listar registros atuais
     children: [
       {path: 'pokemon', redirectTo: 'pokemon/lista', pathMatch: 'full'},
-      {path: 'pokemon/lista', component: TipoListaComponent},
-      {},
+      {path: 'pokemon/create', component: },
     ]
   }
 ];
