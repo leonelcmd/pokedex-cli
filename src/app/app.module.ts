3d71108f41e078e148f6e33b7d677a242694c46c
Leonel Cordova Domagalski <domagalski.leonel@gmail.com>
2021-12-09 09:53:52 -0300

    correção de bugs no routig-module
    to ferrado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { SidebarComponent } from './core/sidebar/sidebar.component';
-import { TipoListaComponent } from './modules/tipo-lista/tipo-lista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-    TipoListaComponent,
   ],
   imports: [
     BrowserModule,
